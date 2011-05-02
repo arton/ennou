5c125ad8c52ff2c3b2ad24eeda0c5fd0c2dbe932
arton <arton@nomad.(none)>
2011-05-03 03:20:55 +0900

    add documents and console bat

---
+++ bin/setenv19.bat
@@ -0,0 +1,16 @@
+@echo off
+"%~dp0ruby" -x "%~f0" %*
+@goto endofruby
+#!/usr/bin/ruby
+require 'winpath'
+path = Pathname.new(File.dirname($0)).shortname.gsub(File::SEPARATOR, File::ALT_SEPARATOR)
+ENV['PATH'] = "#{path};#{ENV['PATH']}"
+Dir.chdir '../..'
+if ARGV.size > 0
+  cmd = 'start ruby "' + ARGV[0] + '"'
+else
+  cmd = "start cmd"
+end
+system(cmd)
+__END__
+:endofruby
